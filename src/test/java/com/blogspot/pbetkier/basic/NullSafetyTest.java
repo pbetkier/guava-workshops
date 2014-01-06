cf53e5c893f597b16190e9345f5ccc7c60c9dcd8
Piotr Betkier <pbetkier@gmail.com>
2014-01-06 22:14:41 +0100

    Added null safety entries.

--- src/test/java/com/blogspot/pbetkier/basic/NullSafetyTest.java
+++ src/test/java/com/blogspot/pbetkier/basic/NullSafetyTest.java
@@ -1,20 +1,40 @@
 package com.blogspot.pbetkier.basic;
 
+import com.google.common.base.Objects;
+import com.google.common.base.Strings;
 import org.junit.Test;
+
+import java.util.Date;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class NullSafetyTest {
 
     @Test
-    public void should() {
+    public void shouldCheckIfNullOrEmptyString() {
+        assertThat(Strings.isNullOrEmpty("")).isTrue();
+        assertThat(Strings.isNullOrEmpty(null)).isTrue();
+        assertThat(Strings.isNullOrEmpty("not empty")).isFalse();
+    }
+
+    @Test
+    public void shouldConvertNullToEmptyString() {
+        assertThat(Strings.nullToEmpty(null)).isEqualTo("");
+        assertThat(Strings.nullToEmpty("other")).isEqualTo("other");
+    }
+
+    @Test
+    public void shouldGiveDefaultValueForNull() {
         // given
+        Date startDate = null;
+        Date currentDate = new Date();
 
         // when
+        Date resolvedStartDate = Objects.firstNonNull(startDate, currentDate);
 
         // then
-
-        // Enum.UNKNOWN
-        // Empty collection / string
-        // Strings.nullOrEmpty
-        // Optional
+        assertThat(resolvedStartDate).isEqualTo(currentDate);
     }
+
+    // Optional
 }
