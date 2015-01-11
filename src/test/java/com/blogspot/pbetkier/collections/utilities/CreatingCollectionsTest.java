e9ac8df2fc8933e2b6e08dc79b2b391e4ecb516c
Piotr Betkier <pbetkier@gmail.com>
2015-01-11 21:50:40 +0100

    Added immutable collections.

--- src/test/java/com/blogspot/pbetkier/collections/utilities/CreatingCollectionsTest.java
+++ src/test/java/com/blogspot/pbetkier/collections/utilities/CreatingCollectionsTest.java
@@ -1,13 +1,12 @@
 package com.blogspot.pbetkier.collections.utilities;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
 import org.assertj.core.util.Lists;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -33,6 +32,42 @@ public class CreatingCollectionsTest {
         // then
         assertThat(createdList).containsExactly("alpha", "beta", "gamma");
         assertThat(createdSet).containsOnly(1, 3, 5);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void shouldCreatePrepopulatedImmutableCollections() {
+        // given
+        List<String> createdList = ImmutableList.of("alpha", "beta", "gamma");
+
+        // throws when
+        createdList.add("disallowed");
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void shouldMakeImmutableCopyOfGivenCollection() {
+        // given
+        Set<String> original = Sets.newHashSet("alpha", "beta", "gamma");
+        Set<String> immutableCopy = ImmutableSet.copyOf(original);
+
+        // throws when
+        immutableCopy.remove("alpha");
+    }
+
+    @Test
+    public void guavaImmutableSetVsJDKUnmodifiableSet() {
+        // given
+        Set<String> original = Sets.newHashSet("alpha", "beta", "gamma");
+
+        Set<String> immutableCopy = ImmutableSet.copyOf(original);
+        Set<String> unmodifiableSet = Collections.unmodifiableSet(original);
+
+
+        // when
+        original.remove("gamma");
+
+        // then
+        assertThat(immutableCopy).containsOnly("alpha", "beta", "gamma");
+        assertThat(unmodifiableSet).containsOnly("alpha", "beta");
     }
 
 }
